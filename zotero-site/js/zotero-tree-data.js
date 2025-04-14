27d9df449b0917cc88fff0e9c1f0c03d05d29730
florestan <warflo@gmail.com>
2025-04-14 18:20:24 +0200

    zot

---
+++ zotero-site/js/zotero-tree-data.js
@@ -0,0 +1,358 @@
+var tree =
+[
+  {
+    "text": "Contact",
+    "item-id": "c201,i16765",
+    "nodes": [
+      {
+        "text": "Caustique mystique",
+        "item-id": "i16765",
+        "nodes": [
+          {
+            "text": "PDF",
+            "item-id": "i16764",
+            "icon": "glyphicon glyphicon-paperclip",
+            "item_title": "PDF",
+            "item_type": "attachment",
+            "item_note": null,
+            "node_type": "item",
+            "metadata": [
+              [
+                "Access Date",
+                "2025-04-14 13:01:27"
+              ],
+              [
+                "Title",
+                "PDF"
+              ],
+              [
+                "URL",
+                "https://www.numdam.org/article/SB_1984-1985__27__19_0.pdf"
+              ]
+            ],
+            "resource": "storage/i16764.pdf"
+          }
+        ],
+        "icon": "glyphicon glyphicon-file",
+        "item_title": "Caustique mystique",
+        "item_type": "journalArticle",
+        "item_note": null,
+        "node_type": "item",
+        "metadata": [
+          [
+            "Creators",
+            "Daniel Bennequin"
+          ],
+          [
+            "Language",
+            "fr"
+          ],
+          [
+            "Library Catalog",
+            "Zotero"
+          ],
+          [
+            "Title",
+            "Caustique mystique"
+          ]
+        ],
+        "resource": "storage/i16764.pdf",
+        "selectable": false
+      },
+      {
+        "text": "Contact geometry the geometrical method of Gibbs theormodynamics.pdf",
+        "item-id": "i1621",
+        "nodes": [
+          {
+            "text": "Arnold - Contact geometry the geometrical method of Gibbs theormodynamics.pdf",
+            "item-id": "i4390",
+            "icon": "glyphicon glyphicon-paperclip",
+            "item_title": "Arnold - Contact geometry the geometrical method of Gibbs theormodynamics.pdf",
+            "item_type": "attachment",
+            "item_note": null,
+            "node_type": "item",
+            "metadata": [
+              [
+                "Title",
+                "Arnold - Contact geometry the geometrical method of Gibbs theormodynamics.pdf"
+              ]
+            ],
+            "resource": "storage/i4390.pdf"
+          }
+        ],
+        "icon": "glyphicon glyphicon-file",
+        "item_title": "Contact geometry the geometrical method of Gibbs theormodynamics.pdf",
+        "item_type": "journalArticle",
+        "item_note": null,
+        "node_type": "item",
+        "metadata": [
+          [
+            "Creators",
+            " Arnol\u02b9d"
+          ],
+          [
+            "Title",
+            "Contact geometry the geometrical method of Gibbs theormodynamics.pdf"
+          ]
+        ],
+        "resource": "storage/i4390.pdf",
+        "selectable": false
+      },
+      {
+        "text": "Mathematical Methods of Classical Mechanics",
+        "item-id": "i15999",
+        "nodes": [
+          {
+            "text": "Full Text PDF",
+            "item-id": "i16000",
+            "icon": "glyphicon glyphicon-paperclip",
+            "item_title": "Full Text PDF",
+            "item_type": "attachment",
+            "item_note": null,
+            "node_type": "item",
+            "metadata": [
+              [
+                "Access Date",
+                "2025-03-23 09:50:14"
+              ],
+              [
+                "Title",
+                "Full Text PDF"
+              ],
+              [
+                "URL",
+                "https://link.springer.com/content/pdf/10.1007%2F978-1-4757-1693-1.pdf"
+              ]
+            ],
+            "resource": "storage/i16000.pdf"
+          },
+          {
+            "text": "Partie contact page 356",
+            "item-id": "n16768",
+            "icon": "glyphicon glyphicon-text-background",
+            "item_title": "Partie contact page 356",
+            "item_type": "note",
+            "item_note": "<div class=\"zotero-note znv1\"><div data-schema-version=\"9\"><p>Partie contact page 356</p>\n</div></div>",
+            "node_type": "note"
+          }
+        ],
+        "icon": "glyphicon glyphicon-book",
+        "item_title": "Mathematical Methods of Classical Mechanics",
+        "item_type": "book",
+        "item_note": null,
+        "node_type": "item",
+        "metadata": [
+          [
+            "Access Date",
+            "2025-03-23 09:50:11"
+          ],
+          [
+            "Creators",
+            "V. I. Arnold, C. C. Moore"
+          ],
+          [
+            "Date",
+            "1978-00-00 1978"
+          ],
+          [
+            "Extra",
+            "DOI: 10.1007/978-1-4757-1693-1"
+          ],
+          [
+            "ISBN",
+            "978-1-4757-1695-5 978-1-4757-1693-1"
+          ],
+          [
+            "Library Catalog",
+            "DOI.org (Crossref)"
+          ],
+          [
+            "Place",
+            "New York, NY"
+          ],
+          [
+            "Publisher",
+            "Springer"
+          ],
+          [
+            "Rights",
+            "http://www.springer.com/tdm"
+          ],
+          [
+            "Series",
+            "Graduate Texts in Mathematics"
+          ],
+          [
+            "Title",
+            "Mathematical Methods of Classical Mechanics"
+          ],
+          [
+            "URL",
+            "http://link.springer.com/10.1007/978-1-4757-1693-1"
+          ],
+          [
+            "Volume",
+            "60"
+          ]
+        ],
+        "resource": "storage/i16000.pdf",
+        "selectable": false
+      },
+      {
+        "text": "Orbites p\u00e9riodiques et courbes pseudo-holomorphes. Application \u00e0 la conjecture de Weinstein en dimension 3",
+        "item-id": "i16661",
+        "nodes": [
+          {
+            "text": "PDF",
+            "item-id": "i16660",
+            "icon": "glyphicon glyphicon-paperclip",
+            "item_title": "PDF",
+            "item_type": "attachment",
+            "item_note": null,
+            "node_type": "item",
+            "metadata": [
+              [
+                "Access Date",
+                "2025-04-11 05:59:12"
+              ],
+              [
+                "Title",
+                "PDF"
+              ],
+              [
+                "URL",
+                "https://www.numdam.org/item/SB_1993-1994__36__309_0.pdf"
+              ]
+            ],
+            "resource": "storage/i16660.pdf"
+          }
+        ],
+        "icon": "glyphicon glyphicon-file",
+        "item_title": "Orbites p\u00e9riodiques et courbes pseudo-holomorphes. Application \u00e0 la conjecture de Weinstein en dimension 3",
+        "item_type": "journalArticle",
+        "item_note": null,
+        "node_type": "item",
+        "metadata": [
+          [
+            "Creators",
+            "Fran\u00e7ois Laudenbach"
+          ],
+          [
+            "Language",
+            "fr"
+          ],
+          [
+            "Library Catalog",
+            "Zotero"
+          ],
+          [
+            "Title",
+            "Orbites p\u00e9riodiques et courbes pseudo-holomorphes. Application \u00e0 la conjecture de Weinstein en dimension 3"
+          ]
+        ],
+        "resource": "storage/i16660.pdf",
+        "selectable": false
+      },
+      {
+        "text": "Topological Methods in 3-Dimensional Contact Geometry",
+        "item-id": "i16628",
+        "nodes": [
+          {
+            "text": "PDF",
+            "item-id": "i16627",
+            "icon": "glyphicon glyphicon-paperclip",
+            "item_title": "PDF",
+            "item_type": "attachment",
+            "item_note": null,
+            "node_type": "item",
+            "metadata": [
+              [
+                "Access Date",
+                "2025-04-11 05:56:19"
+              ],
+              [
+                "Title",
+                "PDF"
+              ],
+              [
+                "URL",
+                "https://www.imo.universite-paris-saclay.fr/~patrick.massot/files/exposition/nantes.pdf"
+              ]
+            ],
+            "resource": "storage/i16627.pdf"
+          }
+        ],
+        "icon": "glyphicon glyphicon-book",
+        "item_title": "Topological Methods in 3-Dimensional Contact Geometry",
+        "item_type": "bookSection",
+        "item_note": null,
+        "node_type": "item",
+        "metadata": [
+          [
+            "Access Date",
+            "2025-04-11 05:56:21"
+          ],
+          [
+            "Book Title",
+            "Contact and Symplectic Topology"
+          ],
+          [
+            "Creators",
+            "Fr\u00e9d\u00e9ric Bourgeois, Vincent Colin, Andr\u00e1s Stipsicz, Patrick Massot"
+          ],
+          [
+            "Date",
+            "2014-00-00 2014"
+          ],
+          [
+            "Extra",
+            "Series Title: Bolyai Society Mathematical Studies\nDOI: 10.1007/978-3-319-02036-5_2"
+          ],
+          [
+            "ISBN",
+            "978-3-319-02035-8 978-3-319-02036-5"
+          ],
+          [
+            "Language",
+            "en"
+          ],
+          [
+            "Library Catalog",
+            "DOI.org (Crossref)"
+          ],
+          [
+            "Pages",
+            "27-83"
+          ],
+          [
+            "Place",
+            "Cham"
+          ],
+          [
+            "Publisher",
+            "Springer International Publishing"
+          ],
+          [
+            "Title",
+            "Topological Methods in 3-Dimensional Contact Geometry"
+          ],
+          [
+            "URL",
+            "http://link.springer.com/10.1007/978-3-319-02036-5_2"
+          ],
+          [
+            "Volume",
+            "26"
+          ]
+        ],
+        "resource": "storage/i16627.pdf",
+        "selectable": false
+      }
+    ],
+    "item_title": "Contact",
+    "item_type": null,
+    "item_note": null,
+    "node_type": "collection",
+    "selectable": false
+  }
+]
